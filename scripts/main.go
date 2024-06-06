de5ae2ffc3e99e92b7b1c2d19e7e0fe1bd2371d5
Chris Wiegman <chris@chriswiegman.com>
2024-06-06 14:00:00 -0500

    Build for multi-arch

--- scripts/main.go
+++ scripts/main.go
@@ -24,7 +24,7 @@ func main() {
 		tag := fmt.Sprintf("%s:%s", dockerOrg, image.Tag)
 
 		dockerCommands := [][]string{
-			{"build", "-t", tag, filepath.Join(imageBasePath, image.Src)},
+			{"buildx", "build", "--platform", "linux/amd64,linux/arm64", "-t", tag, filepath.Join(imageBasePath, image.Src)},
 			{"push", tag},
 		}
 
